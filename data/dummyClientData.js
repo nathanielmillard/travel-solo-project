b3be2b4fbded3974d3a1864573e0134dc1c8b9cb
Nathaniel Millard <nathanielmillard@gmail.com>
2020-09-17 10:54:06 -0600

    Create dummy data for testing.

---
+++ data/dummyClientData.js
@@ -0,0 +1,36 @@
+const travelers = [
+{
+"id": 1,
+"name": "Nathaniel Millard",
+"travelerType": "relaxer"
+},
+{
+"id": 2,
+"name": "Riley Martin",
+"travelerType": "thrill-seeker"
+},
+{
+"id": 3,
+"name": "Christopher Johnson",
+"travelerType": "shopper"
+},
+{
+"id": 4,
+"name": "Leila Thebeaud",
+"travelerType": "photographer"
+},
+{
+{
+"id": 8,
+"name": "Aaron Storm",
+"travelerType": "history buff"
+},
+{
+"id": 16,
+"name": "Larissa Holley",
+"travelerType": "foodie"
+}
+]
+}
+
+module.exports = travelers;
---
+++ data/dummyTripsData.js
@@ -0,0 +1,41 @@
+const trips = [
+{
+"id": 1,
+"userID": 1,
+"destinationID": 1,
+"travelers": 1,
+"date": "2019/09/16",
+"duration": 8,
+"status": "approved",
+"suggestedActivities": []
+},{
+"id": 2,
+"userID": 2,
+"destinationID": 1,
+"travelers": 1,
+"date": "2019/09/16",
+"duration": 8,
+"status": "approved",
+"suggestedActivities": []
+},{
+"id": 3,
+"userID": 3,
+"destinationID": 1,
+"travelers": 1,
+"date": "2019/09/16",
+"duration": 8,
+"status": "approved",
+"suggestedActivities": []
+},{
+"id": 4,
+"userID": 1,
+"destinationID": 2,
+"travelers": 5,
+"date": "2020/10/16",
+"duration": 6,
+"status": "pending",
+"suggestedActivities": []
+}
+]
+
+module.exports = trips;
